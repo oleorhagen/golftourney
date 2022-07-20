c143fb20f1fb8692fdd0632cd684ebff84f853b2
Ole Petter <ole.orhagen@northern.tech>
2022-07-20 21:03:34 +0200

    feat: Initial extra strokes awarded functionality
    The code now calculates the extra strokes awarded, based on the handicap of the
    holes.
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/scorecard/ScoreCardBody.js
+++ ui/src/components/views/tourney/scorecard/ScoreCardBody.js
@@ -5,36 +5,49 @@ import { Paper, Typography, TextField, MenuItem } from "@mui/material";
 
 import ScoreCardTable from "./ScoreCardTable";
 
-const testData = [
+let testData = [
   // Front
-  { par: 5, hcp: 7 },
-  { par: 3, hcp: 11 },
-  { par: 5, hcp: 5 },
-  { par: 4, hcp: 9 },
-  { par: 3, hcp: 15 },
-  { par: 5, hcp: 13 },
-  { par: 3, hcp: 17 },
-  { par: 4, hcp: 3 },
-  { par: 4, hcp: 1 },
+  { hole: 1, par: 5, hcp: 7, extra: 0 },
+  { hole: 2, par: 3, hcp: 11, extra: 0 },
+  { hole: 3, par: 5, hcp: 5, extra: 0 },
+  { hole: 4, par: 4, hcp: 9, extra: 0 },
+  { hole: 5, par: 3, hcp: 15, extra: 0 },
+  { hole: 6, par: 5, hcp: 13, extra: 0 },
+  { hole: 7, par: 3, hcp: 17, extra: 0 },
+  { hole: 8, par: 4, hcp: 3, extra: 0 },
+  { hole: 9, par: 4, hcp: 1, extra: 0 },
   // Back
-  { par: 4, hcp: 16 },
-  { par: 3, hcp: 18 },
-  { par: 4, hcp: 6 },
-  { par: 4, hcp: 14 },
-  { par: 3, hcp: 12 },
-  { par: 5, hcp: 10 },
-  { par: 4, hcp: 8 },
-  { par: 5, hcp: 2 },
-  { par: 4, hcp: 4 },
+  { hole: 10, par: 4, hcp: 16, extra: 0 },
+  { hole: 11, par: 3, hcp: 18, extra: 0 },
+  { hole: 12, par: 4, hcp: 6, extra: 0 },
+  { hole: 13, par: 4, hcp: 14, extra: 0 },
+  { hole: 14, par: 3, hcp: 12, extra: 0 },
+  { hole: 15, par: 5, hcp: 10, extra: 0 },
+  { hole: 16, par: 4, hcp: 8, extra: 0 },
+  { hole: 17, par: 5, hcp: 2, extra: 0 },
+  { hole: 18, par: 4, hcp: 4, extra: 0 },
 ];
 
 // Distribute the number of extra strokes on the available holes. Through a
 // simple distribution of adding strokes to the hardest holes, then continuing..
+// Idea: Sort the array on Hcp, then loop over it until out of extra strokes
 function distributeStrokes(n) {
+  let comparator = (a, b) => {
+    return a.hcp > b.hcp;
+  };
+  let extraStrokes = testData.sort((a, b) => a.hcp > b.hcp);
+  let i = 0;
   while (n > 0) {
-    // Distribute
+    extraStrokes[i % extraStrokes.length].extra++;
+    i++;
+    n--;
   }
+  // Sort by holes again
+  extraStrokes.sort((a, b) => a.hole > b.hole);
+  testData = extraStrokes;
 }
+
+distributeStrokes(25);
 
 export default function ScoreCardBody(props) {
   const scoreCard = props.scorecard;
