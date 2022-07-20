a8249ba1a69ae374c1aa765ed38584784b609251
Ole Petter <ole.orhagen@northern.tech>
2022-07-20 19:22:43 +0200

    feat: align the score to the right in the footer
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/scorecard/ScoreCardFooter.js
+++ ui/src/components/views/tourney/scorecard/ScoreCardFooter.js
@@ -6,7 +6,7 @@ import { Paper, Typography, TextField, MenuItem } from "@mui/material";
 export default function ScoreCardHeader() {
   return (
     <>
-      <Typography variant="h3" component="div" align="left">
+      <Typography variant="h3" component="div" align="right">
         Score:
       </Typography>
     </>
