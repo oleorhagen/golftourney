367cb29f42a12250da175a9b2c968f7391df5599
Ole Petter <ole.orhagen@northern.tech>
2022-07-21 18:32:25 +0200

    chore: Remove the length from the score table
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/scorecard/ScoreCardTable.js
+++ ui/src/components/views/tourney/scorecard/ScoreCardTable.js
@@ -15,8 +15,8 @@ import TableRow from "@mui/material/TableRow";
 
 const allowed_scores = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function createData(hole, par, hcp, length, hcpe) {
-  return { hole, par, hcp, length, hcpe };
+function createData(hole, par, hcp, hcpe) {
+  return { hole, par, hcp, hcpe };
 }
 
 const ScoreCardTable = (props) => {
@@ -27,7 +27,7 @@ const ScoreCardTable = (props) => {
   };
 
   const rows = props.data.map(({ hole, par, hcp, extra }) =>
-    createData(hole, par, hcp, 500, extra)
+    createData(hole, par, hcp, extra)
   );
   return (
     <>
@@ -38,7 +38,6 @@ const ScoreCardTable = (props) => {
               <TableCell>Hole</TableCell>
               <TableCell align="right">Par</TableCell>
               <TableCell align="right">Hcp</TableCell>
-              <TableCell align="right">Length</TableCell>
               <TableCell align="right">Hcp+</TableCell>
               <TableCell align="right">Score</TableCell>
             </TableRow>
@@ -54,7 +53,6 @@ const ScoreCardTable = (props) => {
                 </TableCell>
                 <TableCell align="right">{row.par}</TableCell>
                 <TableCell align="right">{row.hcp}</TableCell>
-                <TableCell align="right">{row.length}</TableCell>
                 <TableCell align="right">
                   <RomanNumeralScore number={row.hcpe} />
                 </TableCell>
