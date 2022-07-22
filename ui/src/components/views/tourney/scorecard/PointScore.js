a304a0b4e550df2511426ada972c2f67329add67
Ole Petter <ole.orhagen@northern.tech>
2022-07-22 11:59:12 +0200

    feat: Track point score in the ScoreTable
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/scorecard/PointScore.js
+++ ui/src/components/views/tourney/scorecard/PointScore.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Typography } from "@mui/material";
 
-// TODO - When we start using this, the Table component needs to keep state (?)
-export default function PointScore({ par }) {
+export default function PointScore({ par, hcp, score, active }) {
+  if (!active) {
+    return null;
+  }
+
+  function points(par, hcp, score) {
+    let res = par + hcp - score + 2;
+    if (res <= 0) {
+      return 0;
+    }
+    return res;
+  }
+
   return (
     <div>
-      <Typography variant="subtitle1">{}</Typography>
+      <Typography variant="subtitle1">{points(par, hcp, score)}</Typography>
     </div>
   );
 }
