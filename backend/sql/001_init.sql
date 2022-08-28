848dfb2a2c102f55039aa94910c850b830669e7f
Ole Petter <ole.orhagen@northern.tech>
2022-08-28 19:46:30 +0200

    chore: Start populating the DB with test data
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- backend/sql/001_init.sql
+++ backend/sql/001_init.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS tournament (
-    id SERIAL PRIMARY KEY,
-    tournament_name STRING NOT NULL
+    id SERIAL8 PRIMARY KEY, tournament_name STRING NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS course (
-    id SERIAL PRIMARY KEY,
-    course_name STRING NOT NULL,
-    -- TODO - Should be an enum type (single, team)
-    style STRING NOT NULL,
+    id
+        SERIAL8 PRIMARY KEY,
+    course_name
+        STRING NOT NULL,
+    style
+        STRING NOT NULL,
     CONSTRAINT tournament_id
         FOREIGN KEY (tournament_id)
         REFERENCES tournament (id)
@@ -15,22 +16,62 @@ CREATE TABLE IF NOT EXISTS course (
 
 CREATE TABLE IF NOT EXISTS tee (
     id
-        SERIAL PRIMARY KEY,
-    name
+        SERIAL8 PRIMARY KEY,
+    tee_name
         STRING NOT NULL,
-    nr
-        INT NOT NULL,
+    -- nr
+    --     INT8 NOT NULL,
     CONSTRAINT course_id
         FOREIGN KEY (course_id) REFERENCES course (id)
 );
 
 CREATE TABLE IF NOT EXISTS hole (
     id
-        SERIAL PRIMARY KEY,
+        SERIAL8 PRIMARY KEY,
     nr
-        INT NOT NULL CHECK (strokes BETWEEN 1 AND 18),
+        INT8 NOT NULL CHECK (strokes BETWEEN 1 AND 18),
+    index
+        INT8 NOT NULL CHECK (strokes BETWEEN 1 AND 18),
     strokes
-        INT NOT NULL CHECK (strokes BETWEEN 1 AND 9),
+        INT8 CHECK (strokes BETWEEN 1 AND 9),
     CONSTRAINT tee_id
         FOREIGN KEY (tee_id) REFERENCES tee (id)
 );
+
+-- TODO - Create a simple populated DB to use for testing
+
+INSERT
+INTO
+    tournament (tournament_name)
+VALUES
+    ('Skjeberg Invitational 2022');
+
+INSERT
+INTO
+    course (tournament_id, course_name)
+VALUES
+    (1, 'Borregaard', 'single'),
+    (1, 'Onsoy', 'team'),
+    (1, 'Gamle Fredrikstad', 'team'),
+    (1, 'Skjeberg', 'single');
+
+
+INSERT
+INTO
+    tee (course_id, tee_name)
+VALUES
+    (1, 'gul'),
+    (1, 'rod'),
+    (1, '34'),
+    (1, '27');
+
+
+
+-- Holes
+
+INSERT
+INTO
+    hole (tee_id, strokes)
+VALUES
+    (1, 1),
+    (1, 2);
