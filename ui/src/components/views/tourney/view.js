f45158c49e2f737531613e04951d747b9175d49b
Ole Petter <ole.orhagen@northern.tech>
2022-10-01 16:16:49 +0200

    feat(graphql): Initial graphql data fetching implemented
    Decided on going with Facebook's Relay client, although slightly complicated to
    use, it looks pretty neat.
    
    Note, as of now, the data is only fetched, and not rendered anywhere!

--- ui/src/components/views/tourney/view.js
+++ ui/src/components/views/tourney/view.js
@@ -4,13 +4,84 @@ import { Container, Grid } from "@mui/material";
 
 import ScoreCard from "./scorecard/ScoreCard";
 
-export const TourneyView = () => {
+import RelayEnvironment from "../../../RelayEnvironment";
+
+import graphql from "babel-plugin-relay/macro";
+
+import {
+  RelayEnvironmentProvider,
+  loadQuery,
+  usePreloadedQuery,
+} from "react-relay/hooks";
+
+// const coursesQuery = graphql`
+//   query viewCoursesQuery {
+//     allCourses {
+//       nodes {
+//         id
+//         name
+//       }
+//     }
+//   }
+// `;
+// const coursesQueryReference = loadQuery(RelayEnvironment, coursesQuery);
+
+const holesQuery = graphql`
+  query viewGetHolesForCourseQuery {
+    allHoles(condition: { courseId: "1" }) {
+      nodes {
+        id
+        index
+        nr
+      }
+    }
+  }
+`;
+const holesQueryReference = loadQuery(RelayEnvironment, holesQuery);
+
+// Inner component that reads the preloaded query results via `usePreloadedQuery()`.
+// This works as follows:
+// - If the query has completed, it returns the results of the query.
+// - If the query is still pending, it "suspends" (indicates to React that the
+//   component isn't ready to render yet). This will show the nearest <Suspense>
+//   fallback.
+// - If the query failed, it throws the failure error. For simplicity we aren't
+//   handling the failure case here.
+function TourneyApp(props) {
+  const data = usePreloadedQuery(holesQuery, props.preloadedQuery);
+
+  console.log(`tourney app: data: ${data}`);
+  console.log(data);
+
+  const {
+    allHoles: { nodes },
+  } = data;
+  const id = nodes[0].id;
+
+  console.log(`id:`);
+  console.log(id);
+
+  return (
+    <div className="TourneyApp">
+      <header className="TourneyApp-header">
+        <p>{id}</p>
+      </header>
+    </div>
+  );
+}
+
+function TourneyView(props) {
   return (
     <>
       <h1>Tourney</h1>
-      <ScoreCard />
+      <RelayEnvironmentProvider environment={RelayEnvironment}>
+        <React.Suspense fallback={"Loading..."}>
+          <TourneyApp preloadedQuery={holesQueryReference} />
+          {/* <ScoreCard /> */}
+        </React.Suspense>
+      </RelayEnvironmentProvider>
     </>
   );
-};
+}
 
 export default TourneyView;
