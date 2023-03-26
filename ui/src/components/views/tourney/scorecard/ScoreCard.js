14e66a3090fc8fc0beeb4795af6165dcefa2826d
Ole Petter <ole.orhagen@northern.tech>
2023-03-26 10:57:26 +0200

    Remove testdata
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/scorecard/ScoreCard.js
+++ ui/src/components/views/tourney/scorecard/ScoreCard.js
@@ -8,44 +8,20 @@ import ScoreCardFooter from "./ScoreCardFooter";
 
 import { PointsFromScore } from "./PointScore";
 
-// INPROGRESS - Get this form the db
-let testData = [
-  // Front
-  { hole: 1, par: 5, hcp: 7, extra: 0 },
-  { hole: 2, par: 3, hcp: 11, extra: 0 },
-  { hole: 3, par: 5, hcp: 5, extra: 0 },
-  { hole: 4, par: 4, hcp: 9, extra: 0 },
-  { hole: 5, par: 3, hcp: 15, extra: 0 },
-  { hole: 6, par: 5, hcp: 13, extra: 0 },
-  { hole: 7, par: 3, hcp: 17, extra: 0 },
-  { hole: 8, par: 4, hcp: 3, extra: 0 },
-  { hole: 9, par: 4, hcp: 1, extra: 0 },
-  // Back
-  { hole: 10, par: 4, hcp: 16, extra: 0 },
-  { hole: 11, par: 3, hcp: 18, extra: 0 },
-  { hole: 12, par: 4, hcp: 6, extra: 0 },
-  { hole: 13, par: 4, hcp: 14, extra: 0 },
-  { hole: 14, par: 3, hcp: 12, extra: 0 },
-  { hole: 15, par: 5, hcp: 10, extra: 0 },
-  { hole: 16, par: 4, hcp: 8, extra: 0 },
-  { hole: 17, par: 5, hcp: 2, extra: 0 },
-  { hole: 18, par: 4, hcp: 4, extra: 0 },
-];
-
 // Distribute the number of extra strokes on the available holes. Through a
 // simple distribution of adding strokes to the hardest holes, then continuing..
 // Idea: Sort the array on Hcp, then loop over it until out of extra strokes
-function distributeStrokes(n) {
-  let extraStrokes = testData.sort((a, b) => a.hcp > b.hcp);
+function distributeStrokes(holes, NumberOfextraStrokes) {
+  let extraStrokes = holes.sort((a, b) => a.hcp > b.hcp);
   let i = 0;
-  while (n > 0) {
+  while (NumberOfextraStrokes > 0) {
     extraStrokes[i % extraStrokes.length].extra++;
     i++;
-    n--;
+    NumberOfextraStrokes--;
   }
   // Sort by holes again
   extraStrokes.sort((a, b) => a.hole > b.hole);
-  testData = extraStrokes;
+  holes = extraStrokes;
 }
 
 // distributeStrokes(25);
@@ -102,7 +78,7 @@ export const ScoreCard = (props) => {
               }
               return PointsFromScore(
                 props.courseData[index].par,
-                testData[index].extra,
+                1, // TODO - How to handle extra strokes now?
                 score
               );
             })
@@ -114,7 +90,7 @@ export const ScoreCard = (props) => {
               }
               return PointsFromScore(
                 props.courseData[index + 9].par,
-                testData[index + 9].extra,
+                1, // TODO - How to handle extra strokes now ?
                 score
               );
             })
