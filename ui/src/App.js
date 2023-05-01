a0c3658f8f4c397cf4d17ac65c14c40a05180a4a
Ole Petter <ole.orhagen@northern.tech>
2023-05-01 14:23:57 +0200

    ui(user-auth): Add aws cognito and amplify user credential sign-in
    This was actually a breeze!
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Auth } from "aws-amplify";
+// TODO
+// (await Auth.currentSession()).getIdToken().getJwtToken()
+
 import React, { useState } from "react";
 
 import { Container } from "@mui/material";
@@ -13,6 +21,15 @@ import ChampionsView from "./components/views/champions/champions";
 import MainView from "./components/views/main/view";
 import TourneyView from "./components/views/tourney/view";
 
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID,
+  },
+});
+
 function App() {
   const [currentView, setView] = useState(MainView);
   const navBarItems = [
@@ -22,18 +39,26 @@ function App() {
   ];
 
   return (
-    <div className="App">
-      <>
-        <NavBar navBarItems={navBarItems} />
-        <Container
-          maxWidth="xl"
-          sx={{ marginTop: (theme) => theme.spacing(4) }}
-        >
-          <main>{currentView}</main>
-        </Container>
-        <Footer />
-      </>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <>
+            <NavBar navBarItems={navBarItems} />
+            <Container
+              maxWidth="xl"
+              sx={{ marginTop: (theme) => theme.spacing(4) }}
+            >
+              <div>
+                <p>Welcome {user.username}</p>
+                <button onClick={signOut}>Sign out</button>
+              </div>
+              <main>{currentView}</main>
+            </Container>
+            <Footer />
+          </>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
---
+++ ui/src/aws-exports.js
@@ -0,0 +1,6 @@
+// replace the user pool region, id, and app client id details
+export default {
+  REGION: "eu-west-1",
+  USER_POOL_ID: "eu-west-1_zAG3Yr5MO",
+  USER_POOL_APP_CLIENT_ID: "5mgolame12tvg2j1lrlet37t3a",
+};
