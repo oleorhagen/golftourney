be84d7ee52363fd957d34d4848b7b87f688b559c
Ole Petter <ole.orhagen@northern.tech>
2023-05-06 12:01:04 +0200

    backend: Start using uuid's for all the ID's in the DB

--- backend/sql/001_init.sql
+++ backend/sql/001_init.sql
@@ -6,33 +6,30 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 
 CREATE TABLE IF NOT EXISTS player (
-  id
-    SERIAL8 PRIMARY KEY,
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   name
     VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS course (
-  id
-    SERIAL8 PRIMARY KEY,
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   name
     VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS hole (
-    id
-        SERIAL8 PRIMARY KEY,
-    course_id SERIAL8 NOT NULL,
-    nr
-        INT8 NOT NULL CHECK (nr BETWEEN 1 AND 18),
-    index
-        INT8 NOT NULL CHECK (index BETWEEN 1 AND 18),
-    par
-        INT8 NOT NULL CHECK (par BETWEEN 1 AND 5),
-    CONSTRAINT course_id
-        FOREIGN KEY (course_id) REFERENCES course (id)
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  course_id uuid NOT NULL,
+  nr
+      INT8 NOT NULL CHECK (nr BETWEEN 1 AND 18),
+  index
+      INT8 NOT NULL CHECK (index BETWEEN 1 AND 18),
+  par
+      INT8 NOT NULL CHECK (par BETWEEN 1 AND 5),
+  CONSTRAINT course_id
+      FOREIGN KEY (course_id) REFERENCES course (id)
 );
 
 
@@ -40,9 +37,9 @@ CREATE TABLE IF NOT EXISTS score (
   strokes INT8 NOT NULL CHECK (strokes BETWEEN 0 AND 9),
   points INT8 NOT NULL CHECK (points BETWEEN 0 AND 9),
   id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  player_id SERIAL8 NOT NULL,
-  course_id SERIAL8 NOT NULL,
-  hole_id SERIAL8 NOT NULL,
+  player_id uuid NOT NULL,
+  course_id uuid NOT NULL,
+  hole_id uuid NOT NULL,
   CONSTRAINT player_id
     FOREIGN KEY (player_id) REFERENCES player (id),
   CONSTRAINT course_id
