a95e4d1fa51b64aa599df6ecbfa9b49f8fb61d63
Ole Petter <ole.orhagen@northern.tech>
2023-05-13 15:15:26 +0200

    Start using the course handicap from the DB
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/playerstats/PlayerStats.js
+++ ui/src/components/views/tourney/playerstats/PlayerStats.js
@@ -2,15 +2,42 @@ import React, { useState } from "react";
 
 import { TextField, Typography, Paper } from "@mui/material";
 
+import graphql from "babel-plugin-relay/macro";
+import { useMutation } from "react-relay";
+
+const PlayerStatsSetHandicapMutation = graphql`
+  mutation PlayerStatsSetHandicapMutation(
+    $handicap: BigInt!
+    $playerId: UUID!
+    $courseId: UUID!
+  ) {
+    createCourseHandicap(
+      input: {
+        courseHandicap: {
+          handicap: $handicap
+          playerId: $playerId
+          courseId: $courseId
+        }
+      }
+    ) {
+      clientMutationId
+    }
+  }
+`;
+
 export default function PlayerStats(props) {
-  const [hcp, setHcp] = useState(localStorage.getItem(props.playerId));
+  const [hcp, setHcp] = useState("");
   const [error, setError] = useState(false);
+
+  const [commitMutation, { createdData, isMutationInFlight }] = useMutation(
+    PlayerStatsSetHandicapMutation
+  );
 
   return (
     <div className="player-state">
       <Paper>
-        <Typography>Player ID: {props.id} </Typography>
-        <Typography>Player HCP: {props.id} </Typography>
+        <Typography>Player ID: {props.playerId} </Typography>
+        <Typography>Player HCP: {hcp} </Typography>
         <TextField
           id="outlined-controlled-hcp-select"
           error={error}
@@ -28,6 +55,13 @@ export default function PlayerStats(props) {
             }
             setError(false);
             setHcp(event.target.value);
+            commitMutation({
+              variables: {
+                handicap: event.target.value,
+                playerId: props.playerId,
+                courseId: props.course_id,
+              },
+            });
             props.onChange(event.target.value);
           }}
         />
