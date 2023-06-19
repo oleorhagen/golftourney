1bcfb752982b6e1d3798d7eb7d5d702ee06586c1
Ole Petter <ole.orhagen@northern.tech>
2023-06-19 21:11:36 +0200

    feat(db): Add a timestamp and a created_at time to the handicaps
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- backend/sql/001_init.sql
+++ backend/sql/001_init.sql
@@ -49,9 +49,12 @@ CREATE TABLE IF NOT EXISTS score (
 );
 
 CREATE TABLE IF NOT EXISTS course_handicap (
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   handicap INT8 NOT NULL CHECK ( handicap BETWEEN 0 AND 54),
   player_id uuid NOT NULL,
   course_id uuid NOT NULL,
+  created_at
+    TIMESTAMP DEFAULT now(),
   CONSTRAINT player_id
     FOREIGN KEY (player_id) REFERENCES player (id),
   CONSTRAINT course_id
