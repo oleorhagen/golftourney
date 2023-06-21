d952e50bd13599ad32c72c5a6a2e6137bdec876a
Ole Petter <ole.orhagen@northern.tech>
2023-06-21 20:49:20 +0200

    chore(ui): Clean up the landing page a little
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/main/view.js
+++ ui/src/components/views/main/view.js
@@ -5,9 +5,9 @@ import { Typography, Skeleton } from "@mui/material";
 const MainView = () => {
   return (
     <>
-      <Typography variant="h3" color="text.secondary" align="center">
+      <Typography variant="h2" color="text.primary" align="center">
         {" "}
-        Welcome to Skjeberg Invitiational {new Date().getFullYear()}
+        Welcome to Skjeberg Invitational {new Date().getFullYear()}
       </Typography>
       {/* For variant="text", adjust the height via font-size */}
       <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
