a764f27272d7e4186379a2191fde6f2e2428aa94
Ole Petter <ole.orhagen@northern.tech>
2023-06-25 12:51:28 +0200

    chore(ui): Wrap the main component in a container
    This nicely aligns everything
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/main/view.js
+++ ui/src/components/views/main/view.js
@@ -1,52 +1,61 @@
 import React from "react";
 
-import { Box, Typography, Paper } from "@mui/material";
+import { Box, Container, Typography, Paper } from "@mui/material";
 
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
-// import "./style.css";
 
 const MainView = () => {
   return (
     <>
-      <Typography variant="h3" color="text.primary" align="center">
-        {" "}
-        Welcome to Skjeberg Invitational {new Date().getFullYear()}
-      </Typography>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignContent="center"
-        alignItems="center"
-        sx={{ margin: 20 }}
-      >
-        <Paper elevation={2}>
-          <Typography
-            sx={{ margin: 5 }}
-            variant="h4"
-            color="text.secondary"
-            align="center"
-          >
+      <Container>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignContent="center"
+          alignItems="center"
+          sx={{ margin: 20 }}
+        >
+          <Typography variant="h3" color="text.primary" align="center">
             {" "}
-            Time left
+            Welcome to Skjeberg Invitational {new Date().getFullYear()}
           </Typography>
-          <FlipClockCountdown
-            to={"2023-06-29T10:00:00"}
-            labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
-            labelStyle={{
-              fontSize: 10,
-              fontWeight: 500,
-              color: "text.secondary",
-            }}
-            digitBlockStyle={{ width: 40, height: 60, fontSize: 30 }}
-            dividerStyle={{ color: "gray", height: 1 }}
-            separatorStyle={{ color: "red", size: "6px" }}
-            duration={0.4}
-          >
-            Finished
-          </FlipClockCountdown>{" "}
-        </Paper>
-      </Box>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignContent="center"
+          alignItems="center"
+          sx={{ margin: 20 }}
+        >
+          <Paper elevation={2}>
+            <Typography
+              sx={{ margin: 5 }}
+              variant="h4"
+              color="text.secondary"
+              align="center"
+            >
+              {" "}
+              Time left
+            </Typography>
+            <FlipClockCountdown
+              to={"2023-06-29T10:00:00"}
+              labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+              labelStyle={{
+                fontSize: 10,
+                fontWeight: 500,
+                color: "text.secondary",
+              }}
+              digitBlockStyle={{ width: 40, height: 60, fontSize: 30 }}
+              dividerStyle={{ color: "gray", height: 1 }}
+              separatorStyle={{ color: "red", size: "6px" }}
+              duration={0.4}
+            >
+              Finished
+            </FlipClockCountdown>{" "}
+          </Paper>
+        </Box>
+      </Container>
     </>
   );
 };
