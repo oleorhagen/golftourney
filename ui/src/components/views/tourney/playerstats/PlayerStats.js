082bad11f0b40cdf15db1991159ec9917504b2b4
Ole Petter <ole.orhagen@northern.tech>
2023-06-25 13:11:17 +0200

    chore(ui): Clean up the schedule interface a little
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/tourney/playerstats/PlayerStats.js
+++ ui/src/components/views/tourney/playerstats/PlayerStats.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 
-import { Button, TextField, Typography, Paper } from "@mui/material";
+import {
+  Button,
+  Chip,
+  Container,
+  TextField,
+  Typography,
+  Paper,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 import graphql from "babel-plugin-relay/macro";
@@ -66,18 +73,74 @@ const HandicapFragment = graphql`
   }
 `;
 
+// function PlayerHandicapExisting(props) {
+//   return (
+//     <Container maxWidth="sm">
+//       <div className="player-state">
+//         <Paper elevation={2} sx={{ marginBottom: 10 }}>
+//           <TextField
+//             id="outlined-controlled-hcp-select"
+//             error={error}
+//             helperText={helpText}
+//             label="Extra Strokes on the Course"
+//             value={hcp || data?.handicap}
+//             onChange={(event) => {
+//               if (isNaN(Number(event.target.value))) {
+//                 setError(true);
+//                 return;
+//               }
+//               const inputHcp = Number(event.target.value);
+//               if (!(inputHcp >= 0 && inputHcp <= 54)) {
+//                 setError(true);
+//                 return;
+//               }
+//               setError(false);
+//               setHcp(event.target.value);
+//             }}
+//           />
+//           <Button
+//             variant="outlined"
+//             onClick={() => {
+//               commitMutation({
+//                 variables: {
+//                   handicap: hcp,
+//                   playerId: props.playerId,
+//                   courseId: props.course_id,
+//                   id: data?.id,
+//                 },
+//                 onError: (e) => {
+//                   console.log(
+//                     `oh nooo, error creating mutation player handicap ${e}`
+//                   );
+//                   setHcp("");
+//                   setError(true);
+//                   setHelpText("Please try again");
+//                 },
+//                 onCompleted: (data) => {
+//                   console.log(
+//                     `successfully updated (completed) the handicap on the server ${data}`
+//                   );
+//                   setHcp(hcp);
+//                   props.onChange(hcp);
+//                   setHelpText("");
+//                 },
+//               });
+//             }}
+//           >
+//             Submit
+//           </Button>
+//         </Paper>
+//       </div>
+//     </Container>
+//   );
+// }
+
 export default function PlayerStats(props) {
   const [hcp, setHcp] = useState("");
   const [error, setError] = useState(false);
-
-  console.log(`player stats props: ${JSON.stringify(props)}`);
-
-  console.log(
-    `got playerStatsHandicapFragment ${JSON.stringify(props.handicap_fragment)}`
-  );
+  const [helpText, setHelpText] = useState("");
 
   const data = useFragment(HandicapFragment, props.handicap_fragment);
-  console.log(`got fragment data: ${JSON.stringify(data)}`);
 
   const [commitMutation, { createdData, isMutationInFlight }] = useMutation(
     props.handicap_fragment
@@ -85,68 +148,63 @@ export default function PlayerStats(props) {
       : PlayerStatsSetHandicapMutation
   );
 
-  if (props.id) {
-    console.log(`Got nodeID`);
-  }
-
-  if (props.id) {
-    console.log(`yay - we got a id to work with now!`);
-  }
-
   return (
-    <div className="player-state">
-      <Paper sx={{ marginBottom: 10 }}>
-        <Typography>Player ID: {props.playerId} </Typography>
-        <Typography>Player HCP: {data?.handicap} </Typography>
-        <TextField
-          id="outlined-controlled-hcp-select"
-          error={error}
-          label="Extra Strokes on the Course"
-          value={hcp}
-          onChange={(event) => {
-            if (isNaN(Number(event.target.value))) {
-              setError(true);
-              return;
-            }
-            const inputHcp = Number(event.target.value);
-            if (!(inputHcp >= 0 && inputHcp <= 54)) {
-              setError(true);
-              return;
-            }
-            setError(false);
-            setHcp(event.target.value);
-          }}
-        />
-        <Button
-          endIcon={<SendIcon />}
-          variant="outlined"
-          onClick={() => {
-            console.log(`button clicked!`);
-            commitMutation({
-              variables: {
-                handicap: hcp,
-                playerId: props.playerId,
-                courseId: props.course_id,
-                id: data?.id,
-              },
-              onError: (e) => {
-                console.log(
-                  `oh nooo, error creating mutation player handicap ${e}`
-                );
-                setHcp("");
-              },
-              onCompleted: (data) => {
-                console.log(
-                  `successfully updated (completed) the handicap on the server ${data}`
-                );
-                props.onChange(hcp);
-              },
-            });
-          }}
-        >
-          Submit
-        </Button>
-      </Paper>
-    </div>
+    <Container maxWidth="sm">
+      <div className="player-state">
+        <Paper elevation={2} sx={{ marginBottom: 10 }}>
+          <TextField
+            id="outlined-controlled-hcp-select"
+            error={error}
+            helperText={helpText}
+            label="Extra Strokes on the Course"
+            value={data?.handicap}
+            onChange={(event) => {
+              if (isNaN(Number(event.target.value))) {
+                setError(true);
+                return;
+              }
+              const inputHcp = Number(event.target.value);
+              if (!(inputHcp >= 0 && inputHcp <= 54)) {
+                setError(true);
+                return;
+              }
+              setError(false);
+              setHcp(event.target.value);
+            }}
+          />
+          <Button
+            variant="outlined"
+            onClick={() => {
+              commitMutation({
+                variables: {
+                  handicap: hcp,
+                  playerId: props.playerId,
+                  courseId: props.course_id,
+                  id: data?.id,
+                },
+                onError: (e) => {
+                  console.log(
+                    `oh nooo, error creating mutation player handicap ${e}`
+                  );
+                  setHcp("");
+                  setError(true);
+                  setHelpText("Please try again");
+                },
+                onCompleted: (data) => {
+                  console.log(
+                    `successfully updated (completed) the handicap on the server ${data}`
+                  );
+                  setHcp(hcp);
+                  props.onChange(hcp);
+                  setHelpText("");
+                },
+              });
+            }}
+          >
+            Submit
+          </Button>
+        </Paper>
+      </div>
+    </Container>
   );
 }
