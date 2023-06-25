cad2ad0338c744972452d2a3cbf90e498074d1fc
Ole Petter <ole.orhagen@northern.tech>
2023-06-25 13:40:30 +0200

    chore(ui): Clean up the champions view
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/champions/champs.js
+++ ui/src/components/views/champions/champs.js
@@ -5,19 +5,6 @@ import "./champs.css";
 import { Paper, Typography } from "@mui/material";
 
 export const PreviousChampion = (props) => {
-  // TODO - Layout should be a grid with champs
-  // The latest champion should wear a crown
-  // Every champion should have an img (this I have for both years)
-  // And the name is shining lights
-  // Johnny b good' to'nite
-  // Arrow pointing to the latest champ, saying: `This Is the Champ!!!`
-
-  //
-  // TODO - How does HTML img automatically choose the png size ? I don't remember
-  // https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images - Look at responsive images here
-  //
-
-  // TODO - Generate multiple formats of the images used, the one currently is too big!
   return (
     <Paper elevation={3}>
       <Typography gutterBottom variant="h1" component="div" align="center">
