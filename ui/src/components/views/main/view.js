47efad02cdf86710b2b7375f5b9e8c86ed22ad8c
Ole Petter <ole.orhagen@northern.tech>
2023-06-25 09:29:14 +0200

    feat(ui): Add a countdown clock on the front page
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/main/view.js
+++ ui/src/components/views/main/view.js
@@ -1,20 +1,68 @@
 import React from "react";
 
-import { Typography, Skeleton } from "@mui/material";
+import { Typography, Paper, List, ListItem, ListItemText } from "@mui/material";
+
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+
+const courses = [
+  {
+    day: 1,
+    course: "Borregaard",
+    type: "single",
+  },
+  {
+    day: 2,
+    course: "Gamle Fredrikstad",
+    type: "single",
+  },
+  {
+    day: 3,
+    course: "Skjeberg",
+    type: "single",
+  },
+  {
+    day: 4,
+    course: "Onsoy",
+    type: "single",
+  },
+];
 
 const MainView = () => {
   return (
     <>
-      <Typography variant="h2" color="text.primary" align="center">
+      <Typography variant="h3" color="text.primary" align="center">
         {" "}
         Welcome to Skjeberg Invitational {new Date().getFullYear()}
       </Typography>
-      {/* For variant="text", adjust the height via font-size */}
-      <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
-      {/* For other variants, adjust the size with `width` and `height` */}
-      <Skeleton variant="circular" width={40} height={40} />
-      <Skeleton variant="rectangular" width={210} height={60} />
-      <Skeleton variant="rounded" width={210} height={60} />{" "}
+      <FlipClockCountdown to={new Date("2023-06-29T10:00:00")} />
+      <Paper>
+        <Typography variant="h4">Format:</Typography>
+        <Typography variant="p">
+          Skjeberg Invitational is a four day tournament hosted at the given
+          courses:
+        </Typography>
+        <ul>
+          {courses.map((n, i) => (
+            <li>
+              {n.course} on day {n.day}
+            </li>
+          ))}
+        </ul>
+      </Paper>
+      <Paper>
+        <Typography variant="h4">Rules:</Typography>
+        <Typography variant="p">
+          Regular rules of Golf, with the following additions
+        </Typography>
+        <ul>
+          {courses.map((n, i) => (
+            <li>
+              {n.course} on day {n.day}
+            </li>
+          ))}
+        </ul>
+      </Paper>
     </>
   );
 };
