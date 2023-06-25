15c2ec05f251f4fb760ece0f917602624cea78bc
Ole Petter <ole.orhagen@northern.tech>
2023-06-25 21:37:29 +0200

    feat(ui): Pretty up the Champions view
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/champions/champions.js
+++ ui/src/components/views/champions/champions.js
@@ -12,11 +12,23 @@ export const Champions = () => {
       name: "Marius Sollie",
       img: champImg22,
       text: "The winner of the 2022 Skjeberg Invitational",
+      avatarName: "MS",
+      bgColor: "red",
+      date: "July 02, 2022",
+      short: "Iceman",
+      story:
+        "After an impressive show of keeping his cool, and the nerves in check, Marius came out on top of the 2022 Skjeberg Invitational. Being chased down the final leg by the venerable Juliane Karling, Marius was never really in trouble, and after making it over the water on 17, and his opponents getting into some trouble, Marius could enjoy the walk down 18th at Skjeberg. Congratulations!",
     },
     {
       name: "Ole M. Hellerud",
       img: champImg21,
       text: "The winner of the 2021 Skjeberg Open",
+      avatarName: "OH",
+      bgColor: "blue",
+      date: "July 02, 2021",
+      short: "McLane?",
+      story:
+        "Being chased down the final day at the final boss (Skjeberg), Ole M., keept his opponents at bay, and became the first (original) winner of the Skjeberg (then Open).",
     },
   ];
 
@@ -28,11 +40,7 @@ export const Champions = () => {
           <Grid container spacing={2}>
             {previousChamps.map((champ) => (
               <Grid key={champ.name} item>
-                <PreviousChampion
-                  name={champ.name}
-                  img={champ.img}
-                  text={champ.text}
-                />
+                <PreviousChampion {...champ} />
               </Grid>
             ))}
           </Grid>
