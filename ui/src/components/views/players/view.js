1ce230983c5fe2a3c3dd245a064843cbda4bd076
Ole Petter <ole.orhagen@northern.tech>
2023-06-29 21:22:28 +0200

    feat(ui): Add the avatars and logo
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- ui/src/components/views/players/view.js
+++ ui/src/components/views/players/view.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useOutletContext } from "react-router-dom";
+
 import { Avatar, Paper, Stack, Typography, Skeleton } from "@mui/material";
 
 const players = [
@@ -8,12 +10,14 @@ const players = [
     hcp: 20,
     sex: "male",
     bio: "Ole Petter is the host of the Skjeberg Invitational (formerly Skjeberg Open), and an avid recreational golfer.",
+    avatarImg: "/static/images/avatar/626fa9fd-95ed-40e8-90f3-139ec79e79b9.jpg",
   },
   {
     name: "Ole Martin Hellerud",
     hcp: 29,
     sex: "male",
     bio: "Ole Martin was the first winner of the Skjeberg open, and..",
+    avatarImg: "/static/images/avatar/47e28a8a-8aab-4f5d-a4b4-3c2151514ba6.jpg",
   },
 
   {
@@ -21,20 +25,22 @@ const players = [
     hcp: 20.1,
     sex: "male",
     bio: "Marius is the reigning and defending champion at this years Skjeberg Invitational",
+    avatarImg: "/static/images/avatar/9a771ff0-a6f6-462d-b66a-0e2f66ffb21b.jpg",
   },
 
   {
     name: "Juliane S. Karling",
     hcp: 20.1,
     sex: "female",
-    bio: "Juliane is the challenger, and runner up from last years Skjeberg invitational",
+    bio: "Kjempe smellvakker prinsesse beauty queen <3",
+    avatarImg: "/static/images/avatar/a892ead4-9d58-4e0f-8111-ce17b2f364e4.jpg",
   },
 ];
 
 const Player = (props) => {
   return props.name ? (
     <Paper>
-      <Avatar alt={props.name} src="/static/images/avatar/logo512.png" />
+      <Avatar alt={props.name} src={props.avatarImg} />
       <Typography variant="h4" color="text.primary" align="left">
         Name: {props.name}
       </Typography>
@@ -66,6 +72,7 @@ const Player = (props) => {
 
 // TODO - Get the players info from the DB
 const PlayersView = () => {
+  const [playerId] = useOutletContext();
   return (
     <>
       <Typography variant="h1" color="text.primary" align="center">
