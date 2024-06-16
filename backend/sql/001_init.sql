2d3fa14ef4b2e4d129a1de8064fa19731e36398c
Ole P. Orhagen <ole.petter.orhagen@mimiro.no>
2024-06-16 09:41:10 +0200

    db: Update the unique constrain for hole
    Making sure that the index in unique, in the context of a course, ensuring no
    duplicate indexes.
    
    Also prettied up the schema syntax.
    
    Signed-off-by: Ole P. Orhagen <ole.petter.orhagen@mimiro.no>

--- backend/sql/001_init.sql
+++ backend/sql/001_init.sql
@@ -6,35 +6,45 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE SCHEMA IF NOT EXISTS physical AUTHORIZATION postgres;
 
 CREATE TABLE physical.tournament (
-	id UUID DEFAULT uuid_generate_v4(),
-  name VARCHAR(50) NOT NULL,
-  year DATE NOT NULL,
+	id
+    UUID DEFAULT uuid_generate_v4(),
+  name
+    VARCHAR(50) NOT NULL,
+  year
+    DATE NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE physical.scorer (
-id
-UUID DEFAULT uuid_generate_v4(),
-name
-VARCHAR(50) NOT NULL,
-PRIMARY KEY (id)
+  id
+    UUID DEFAULT uuid_generate_v4(),
+  name
+    VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
 );
 
+create domain valid_handicap as int8
+CHECK
+  (VALUE BETWEEN -54 AND 54);
+
 CREATE TABLE physical.player (
-id UUID,
-handicap int8 NOT NULL CHECK (handicap BETWEEN -54 AND 54),
-PRIMARY KEY (id),
-FOREIGN KEY (id)
-REFERENCES physical.scorer (id) ON DELETE CASCADE
+  id
+    UUID,
+  handicap
+    valid_handicap NOT NULL ,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id)
+    REFERENCES physical.scorer (id) ON DELETE CASCADE
 );
 
 -- TODO - Create a team view which calculates the handicap as the average of the
 -- teams members handicap
 CREATE TABLE physical.team (
-id UUID,
-PRIMARY KEY (id),
-FOREIGN KEY (id)
-REFERENCES physical.scorer (id) ON DELETE CASCADE
+  id
+    UUID,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id)
+    REFERENCES physical.scorer (id) ON DELETE CASCADE
 );
 
 CREATE TABLE physical.team_member (
@@ -44,8 +54,9 @@ CREATE TABLE physical.team_member (
     UUID,
   PRIMARY KEY (player_id, team_id),
   FOREIGN KEY (player_id)
-  REFERENCES physical.player (id) ON DELETE CASCADE,
-  FOREIGN KEY (team_id) REFERENCES physical.team (id) ON DELETE CASCADE
+    REFERENCES physical.player (id) ON DELETE CASCADE,
+  FOREIGN KEY (team_id)
+    REFERENCES physical.team (id) ON DELETE CASCADE
 );
 
 CREATE TABLE physical.competition_score (
@@ -57,17 +68,22 @@ CREATE TABLE physical.competition_score (
     INT8 NOT NULL,
   PRIMARY KEY (tournament_id, player_id),
   FOREIGN KEY (tournament_id)
-  REFERENCES physical.tournament (id)
-  ON DELETE CASCADE,
+    REFERENCES physical.tournament (id)
+      ON DELETE CASCADE,
   FOREIGN KEY (player_id)
-  REFERENCES physical.player (id) ON DELETE CASCADE
+    REFERENCES physical.player (id)
+      ON DELETE CASCADE
 );
 
 CREATE TABLE physical.course (
-	name VARCHAR(50),
-  slope float NOT NULL, -- Indicates how difficult the course is expected to be for a bogey golfer
-  course_rating float NOT NULL, -- The number of strokes a scratch is expected to use
-  nr_holes INT8 NOT NULL,
+	name
+    VARCHAR(50),
+  slope
+    float NOT NULL, -- Indicates how difficult the course is expected to be for a bogey golfer
+  course_rating
+    float NOT NULL, -- The number of strokes a scratch is expected to use
+  nr_holes
+    INT8 NOT NULL,
 	PRIMARY KEY (name)
 );
 
@@ -77,12 +93,13 @@ CREATE TABLE physical.course_hole (
 	course_name
 		VARCHAR(50),
 	hole_index
-		INT8 NOT NULL CHECK (hole_index BETWEEN 1 AND 18), -- TODO make sure this IC has unique hole_index
+		INT8 NOT NULL CHECK (hole_index BETWEEN 1 AND 18),
 	par
 		INT8 NOT NULL CHECK (par BETWEEN 1 AND 5) NOT NULL,
 	PRIMARY KEY (hole_nr, course_name),
 	FOREIGN KEY (course_name)
-		REFERENCES physical.course (name) ON DELETE CASCADE
+		REFERENCES physical.course (name) ON DELETE CASCADE,
+  UNIQUE (hole_nr, course_name, hole_index)
 );
 
 CREATE TABLE physical.hole_score (
