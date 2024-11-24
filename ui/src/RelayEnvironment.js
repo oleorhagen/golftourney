d260d9610d4bcbdab7e0d05663ced9aee953ef36
Ole P. Orhagen <ole.petter.orhagen@mimiro.no>
2024-11-24 03:52:42 +0100

    chore(ws): Add the subscribe param to the environment
    Signed-off-by: Ole P. Orhagen <ole.petter.orhagen@mimiro.no>

--- ui/src/RelayEnvironment.js
+++ ui/src/RelayEnvironment.js
@@ -38,6 +38,6 @@ async function fetchRelay(params, variables) {
 
 // Export a singleton instance of Relay Environment configured with our network function:
 export default new Environment({
-  network: Network.create(fetchRelay),
+  network: Network.create(fetchRelay, subscribe),
   store: new Store(new RecordSource()),
 });
