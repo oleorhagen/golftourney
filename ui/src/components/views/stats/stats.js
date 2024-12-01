a1df4e160ee0dfffad605ae6835aa63f08c69efd
Ole P. Orhagen <ole@orhagen.no>
2024-12-01 21:19:50 +0100

    chore(ui/charts): Have Par and Total points graphs render proper
    Otherwise sometimes they get squashed a little.
    
    Signed-off-by: Ole P. Orhagen <ole@orhagen.no>

--- ui/src/components/views/stats/stats.js
+++ ui/src/components/views/stats/stats.js
@@ -17,51 +17,6 @@ import { memo, useState } from "react";
 
 import { useLazyLoadQuery } from "react-relay/hooks";
 
-// const data = [
-//   {
-//     name: "Page A",
-//     uv: 4000,
-//     pv: 2400,
-//     amt: 2400,
-//   },
-//   {
-//     name: "Page B",
-//     uv: 3000,
-//     pv: 1398,
-//     amt: 2210,
-//   },
-//   {
-//     name: "Page C",
-//     uv: 2000,
-//     pv: 9800,
-//     amt: 2290,
-//   },
-//   {
-//     name: "Page D",
-//     uv: 2780,
-//     pv: 3908,
-//     amt: 2000,
-//   },
-//   {
-//     name: "Page E",
-//     uv: 1890,
-//     pv: 4800,
-//     amt: 2181,
-//   },
-//   {
-//     name: "Page F",
-//     uv: 2390,
-//     pv: 3800,
-//     amt: 2500,
-//   },
-//   {
-//     name: "Page G",
-//     uv: 3490,
-//     pv: 4300,
-//     amt: 2100,
-//   },
-// ];
-
 const GetStatisticsQuery = graphql`
   query statsGetQuery($tournamentId: UUID!) {
     statistics(filter: { tournamentId: { equalTo: $tournamentId } }) {
@@ -97,17 +52,16 @@ const ScoreChart = memo(function ScoreChart({ data }) {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <YAxis />
-        <XAxis dataKey="expectedPoints" />
+        <YAxis type="integer" />
+        <XAxis />
         <Tooltip />
         <Legend />
         <Line
-          type="monotone"
-          dataKey="expectedPoints"
-          stroke="#8884d8"
+          type="basic"
+          dataKey="totalPoints"
+          stroke="#82ca9d"
           activeDot={{ r: 8 }}
         />
-        // <Line type="monotone" dataKey="totalPoints" stroke="#82ca9d" />
       </LineChart>
     </ResponsiveContainer>
   );
