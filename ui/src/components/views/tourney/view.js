ee7199c49a2864864aacc7ceb7a3287bc229169d
Ole P. Orhagen <ole@orhagen.no>
2025-07-16 12:00:53 +0200

    chore: Add support for creating scorecards

--- ui/src/components/views/tourney/view.js
+++ ui/src/components/views/tourney/view.js
@@ -4,7 +4,8 @@ import graphql from "babel-plugin-relay/macro";
 import { useOutletContext, Outlet } from "react-router-dom";
 import Link from "../../router/Link";
 
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Tab, Tabs, Typography, Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField, MenuItem } from "@mui/material";
+import { useMutation } from "react-relay/hooks";
 
 // import PlayerStats from "./playerstats/PlayerStats";
 import ScoreCard from "./scorecard/ScoreCard";
@@ -41,6 +42,39 @@ const ListAllScorecardsQuery = graphql`
         handicap
       }
     }
+    courses {
+      name
+    }
+  }
+`;
+
+const CreateScorecardMutation = graphql`
+  mutation viewCreateScorecardMutation($input: NewScorecard!) {
+    createScorecard(input: $input) {
+      id
+      tournament_id
+      handicap
+      course {
+        name
+        nr_holes
+        slope
+        course_rating
+        holes {
+          nr
+          index
+          par
+          extra_strokes
+          strokes
+          points
+          ...SelectScoreAutoWidthFragment
+        }
+      }
+      player {
+        id
+        name
+        handicap
+      }
+    }
   }
 `;
 
@@ -51,6 +85,9 @@ export function RouterScoreCard() {
 
 function ScheduleScoreCard(props) {
   const [value, setValue] = useState(0);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [courseName, setCourseName] = useState("");
+  const [handicap, setHandicap] = useState("");
 
   const data = useLazyLoadQuery(
     ListAllScorecardsQuery,
@@ -58,16 +95,44 @@ function ScheduleScoreCard(props) {
     { fetchPolicy: "network-only" },
   );
 
+  const [createScorecard] = useMutation(CreateScorecardMutation);
+
   var courseNodes = data?.scorecards || [];
+  var availableCourses = data?.courses || [];
 
   const handleTabChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const handleCreateScorecard = () => {
+    createScorecard({
+      variables: {
+        input: {
+          tournament_id: props.tournamentId,
+          player_id: props.scorerId,
+          course_name: courseName,
+          handicap: parseInt(handicap)
+        }
+      },
+      onCompleted: () => {
+        setDialogOpen(false);
+        setCourseName("");
+        setHandicap("");
+      }
+    });
+  };
+
   return (
     <div className="TourneyApp">
       <div className="TourneyApp-header">
-        <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <Box sx={{ width: "100%", bgcolor: "background.paper", mb: 2 }}>
+          <Button
+            variant="contained"
+            onClick={() => setDialogOpen(true)}
+            sx={{ mb: 2 }}
+          >
+            Create New Scorecard
+          </Button>
           <Tabs
             value={value}
             onChange={handleTabChange}
@@ -101,6 +166,44 @@ function ScheduleScoreCard(props) {
             </CustomTabPanel>
           );
         })}
+
+        <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)} maxWidth="sm" fullWidth>
+          <DialogTitle>Create New Scorecard</DialogTitle>
+          <DialogContent>
+            <TextField
+              select
+              label="Course"
+              value={courseName}
+              onChange={(e) => setCourseName(e.target.value)}
+              fullWidth
+              margin="normal"
+            >
+              {availableCourses.map((course) => (
+                <MenuItem key={course.name} value={course.name}>
+                  {course.name}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              label="Handicap"
+              type="number"
+              value={handicap}
+              onChange={(e) => setHandicap(e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setDialogOpen(false)}>Cancel</Button>
+            <Button
+              onClick={handleCreateScorecard}
+              variant="contained"
+              disabled={!courseName || !handicap}
+            >
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </div>
   );
